f76cbb5bf65c105eac32ccd369a430f0e0bae7cf
Sara Ward <saro4cka.ward@yandex.com>
2021-10-17 17:50:28 +0100

    add: js for popup form

---
+++ script.js
@@ -0,0 +1,43 @@
+let editFormEl = document.querySelector('.popup__form');
+
+let modalEl = document.querySelector('.popup');
+
+let editButton = document.querySelector(".profile__edit-button");
+
+let closeButton = document.querySelector('.popup__close-button');
+
+let nameInput = document.querySelector('#name');
+let occupationInput = document.querySelector('#occupation');
+
+let name = document.querySelector('.popup__input_name');
+let occupation = document.querySelector('.profile__description');
+
+console.log(editButton);
+
+function handleFormOpen() {
+  modalEl.classList.remove('popup__closed');
+  nameInput.value = name.textContent;
+  occupationInput.value = occupation.textContent;
+}
+
+function handleFormClose() {
+  modalEl.classList.add('popup__closed');
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  nameInputValue = nameInput.value;
+  occupationInputValue = occupationInput.value;
+
+  name.textContent = nameInputValue;
+  occupation.textContent = occupationInputValue;
+
+  handleFormClose();
+}
+
+editButton.addEventListener('click', handleFormOpen);
+
+closeButton.addEventListener('click', handleFormClose);
+
+editFormEl.addEventListener('submit', handleFormSubmit);
