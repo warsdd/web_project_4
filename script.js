16a913263e06dcd60f29f9ddb48319817684ecfe
Sara Ward <saro4cka.ward@yandex.com>
2021-10-17 18:08:29 +0100

    edit: popup__opened & add: syntax changes

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let editFormEl = document.querySelector('.popup__form');
+let editFormEl = document.querySelector(".popup__form");
 
-let modalEl = document.querySelector('.popup');
+let modalEl = document.querySelector(".popup");
 
 let editButton = document.querySelector(".profile__edit-button");
 
-let closeButton = document.querySelector('.popup__close-button');
+let closeButton = document.querySelector(".popup__close-button");
 
-let nameInput = document.querySelector('#name');
-let occupationInput = document.querySelector('#occupation');
+let nameInput = document.querySelector("#name");
+let occupationInput = document.querySelector("#occupation");
 
-let name = document.querySelector('.popup__input_name');
-let occupation = document.querySelector('.profile__description');
+let name = document.querySelector(".popup__input_name");
+let occupation = document.querySelector(".profile__description");
 
 console.log(editButton);
 
 function handleFormOpen() {
-  modalEl.classList.remove('popup__closed');
+  modalEl.classList.remove("popup__closed");
   nameInput.value = name.textContent;
   occupationInput.value = occupation.textContent;
 }
 
 function handleFormClose() {
-  modalEl.classList.add('popup__closed');
+  modalEl.classList.add("popup__closed");
 }
 
 function handleFormSubmit(evt) {
@@ -36,8 +36,8 @@ function handleFormSubmit(evt) {
   handleFormClose();
 }
 
-editButton.addEventListener('click', handleFormOpen);
+editButton.addEventListener("click", handleFormOpen);
 
-closeButton.addEventListener('click', handleFormClose);
+closeButton.addEventListener("click", handleFormClose);
 
-editFormEl.addEventListener('submit', handleFormSubmit);
+editFormEl.addEventListener("submit", handleFormSubmit);
